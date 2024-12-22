101d27d9a4cdd2a85d93a0505d9d05c426f2ba93
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-22 22:26:47 +0100

    Removed: gpu request from sbatch file

--- eval_random_baseline.sh
+++ eval_random_baseline.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #SBATCH --time=6:00:00 # never more than 6 hours
 #SBATCH --cpus-per-task=1  # must match num_env, never more than 16
-#SBATCH --gres=gpu:0  # never more than 0
 #SBATCH --partition=performance # do not change
 #SBATCH --output=out/random-baseline-%A_%a.out
 #SBATCH --error=out/random-baseline-%A_%a.err
