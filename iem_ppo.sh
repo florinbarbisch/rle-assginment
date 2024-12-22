c547a808c42adb5b15f0b982bb0a1a803f9cb6a4
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-22 23:48:36 +0100

    Added: iem_ppo

---
+++ iem_ppo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --time=6:00:00 # never more than 6 hours
+#SBATCH --cpus-per-task=16  # must match num_env, never more than 16
+#SBATCH --gres=gpu:0  # never more than 0
+#SBATCH --partition=performance # do not change
+#SBATCH --output=out/iem-ppo-%A_%a.out
+#SBATCH --error=out/iem-ppo-%A_%a.err
+
+.venv/bin/python iem_ppo.py --track --exp_name iem_ppo
