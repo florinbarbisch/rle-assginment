5d00074f4f48aed0c385283b344e1542548d0be8
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-22 22:32:05 +0100

    fixed naming

--- initial_run.sh
+++ initial_run.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=16  # must match num_env, never more than 16
 #SBATCH --gres=gpu:0  # never more than 0
 #SBATCH --partition=performance # do not change
-#SBATCH --output=out/ppo-clean-rl-%A_%a.out
-#SBATCH --error=out/ppo-clean-rl-%A_%a.err
+#SBATCH --output=out/initial-run-%A_%a.out
+#SBATCH --error=out/initial-run-%A_%a.err
 
 .venv/bin/python ppo_clean_rl.py --track --exp_name initial_run
