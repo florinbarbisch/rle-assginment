5c53e2baf0c954a06cc86443c814ba01d55286aa
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-22 21:29:44 +0100

    Added: slurm scripts

---
+++ ppo_clean_rl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --time=6:00:00 # never more than 6 hours
+#SBATCH --cpus-per-task=16  # must match num_env, never more than 16
+#SBATCH --gres=gpu:0  # never more than 0
+#SBATCH --partition=performance # do not change
+#SBATCH --output=out/ppo-clean-rl-%A_%a.out
+
+.venv/bin/activate
+python ppo_clean_rl.py --track
