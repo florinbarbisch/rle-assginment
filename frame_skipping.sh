786208f341cb08b86e979759be889b365b0f0fba
florinbarbisch <40719099+florinbarbisch@users.noreply.github.com>
2024-12-23 12:46:25 +0100

    Fixed: frame skipping got timeout

--- frame_skipping.sh
+++ frame_skipping.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --time=6:00:00 # never more than 6 hours
+#SBATCH --time=12:00:00 # never more than 6 hours
 #SBATCH --cpus-per-task=16  # must match num_env, never more than 16
 #SBATCH --gres=gpu:0  # never more than 0
 #SBATCH --partition=performance # do not change
